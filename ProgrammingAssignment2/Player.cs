1f2439bcc6ef728f028b7ac8ea7f143e7a2bd43d
puglisac <alan.c.puglisi@gmail.com>
2020-11-24 15:51:36 -0600

    completed assginment

---
+++ ProgrammingAssignment2/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleCards;
+namespace ProgrammingAssignment2
+{
+    public class Player
+    {
+        public string Name { get; set;}
+        public List<Card> CurrentHand { get; set; }
+
+        public Player(string name)
+        {
+            Name = name;
+            CurrentHand = new List<Card>();
+        }
+        public void drawCard(Card card)
+        {
+            CurrentHand.Add(card);
+        }
+        public void flipCards()
+        {
+            foreach (Card card in CurrentHand)
+            {
+                card.FlipOver();
+            }
+        }
+        public void showCards()
+        {
+            Console.WriteLine($"{Name} has: ");
+            foreach(Card card in CurrentHand)
+            {
+                Console.WriteLine($"{card.Rank} of {card.Suit}");
+            }
+        }
+    }
+}
---
+++ ProgrammingAssignment2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using ConsoleCards;
+
+namespace ProgrammingAssignment2
+{
+    // IMPORTANT: Only add code in the section
+    // indicated below. The code I've provided
+    // makes your solution work with the 
+    // automated grader on Coursera
+
+    /// <summary>
+    /// Programming Assignment 2
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Implements Nothing Like Blackjack functionality
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+
+
+            Player player1 = new Player("Player 1");
+            Player player2 = new Player("Player 2");
+            Player player3 = new Player("Player 3");
+            Player player4 = new Player("Player 4");
+
+
+            // Add your code between this comment
+            // and the comment below. You can of
+            // course add more space between the
+            // comments as needed
+
+            // declare a deck variable and create a deck object
+            Deck deck = new Deck();
+            // DON'T SHUFFLE THE DECK
+
+            // deal 2 cards each to 4 players (deal properly, dealing
+            // the first card to each player before dealing the
+            // second card to each player)
+            for (var i = 2; i > 0; i--)
+            {
+                player1.drawCard(deck.TakeTopCard());
+                player2.drawCard(deck.TakeTopCard());
+                player3.drawCard(deck.TakeTopCard());
+                player4.drawCard(deck.TakeTopCard());
+            }
+            // deal 1 more card to players 2 and 3
+                player2.drawCard(deck.TakeTopCard());
+                player3.drawCard(deck.TakeTopCard());
+
+            // flip all the cards over
+            player1.flipCards();
+            player2.flipCards();
+            player3.flipCards();
+            player4.flipCards();
+
+            // print the cards for player 1
+            player1.showCards();
+
+            // print the cards for player 2
+            player2.showCards();
+
+            // print the cards for player 3
+            player3.showCards();
+
+            // print the cards for player 4
+            player4.showCards();
+
+        }
+    }
+}
